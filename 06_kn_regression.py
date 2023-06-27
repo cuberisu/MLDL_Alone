ca8a2b584eafe1a2af791dadafec79fd8acb7994
cuberisu <cuberisu@gmail.com>
2023-06-27 20:54:38 +0900

    add anotation

--- 06_kn_regression.py
+++ 06_kn_regression.py
@@ -43,28 +43,38 @@ test_array = test_array.reshape(2, 2)
 print(test_array.shape) # (2, 2)
 
 # test_array = test_array.reshape(2, 3) 이거는 에러가 발생한다.
+
+# 열 차원을 1로 두고 행 차원을 -1로 둔다는 것은 
+# 나머지 차원이 다 결정되고 남은 차원을 사용하겠다는 뜻
+# 특성이 열 방향으로 있어야 하기 때문에 이렇게 한다.
 train_input = train_input.reshape(-1, 1)
 test_input = test_input.reshape(-1, 1)
 print(train_input.shape, test_input.shape)  # (42, 1) (14, 1)
+# target 데이터는 1차원 함수로 두어도 된다.
 
-
-# regression에서의 score
+# regression에서의 score는 결정 계수 R^2
+# 1에 가까울수록 좋다
 from sklearn.neighbors import KNeighborsRegressor
 knr = KNeighborsRegressor()
 knr.fit(train_input, train_target) # k-최근접 이웃 회귀 모델을 훈련
 print(knr.score(test_input, test_target)) # 0.992809406101064
 
-# 결정 계수 R^2
+# 다른 지표: 평균절댓값오차
 from sklearn.metrics import mean_absolute_error
 test_prediction = knr.predict(test_input) # 테스트 세트에 대한 예측
 mae = mean_absolute_error(test_target, test_prediction) # 테스트 세트에 대한 평균 절댓값 오차 계산
-print(mae)    # 19.157142857142862
+print(mae)    # 19.157142857142862 19g 정도 오차난다는 뜻
 
 
-# 과대 적합 vs 과소 적합
+# 과대 적합 underfitting vs 과소 적합 overfitting
+# 일반적으로 훈련 세트의 점수가 테스트세트 점수가 높아야 한다.
+# 아래는 과소적합
 print(knr.score(train_input, train_target))      # 0.9698823289099254
+print(knr.score(train_input, train_target))      # 0.9928094061010639
 
+# 이웃의 개수가 늘어나면 과소적합, 줄어들면 과대적합
 knr.n_neighbors = 3  # 이웃의 개수를 3으로 설정
+# 우리가 지정해야 하는 매개변수: 하이퍼파라미터
 knr.fit(train_input, train_target)        # 모델을 재훈련
 print(knr.score(train_input, train_target))      # 0.9804899950518966
 print(knr.score(test_input, test_target))        # 0.9746459963987609
