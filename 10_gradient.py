e533033496445ae713fe54d50ef2d67835624b71
cuberisu <cuberisu@gmail.com>
2023-06-28 22:21:45 +0900

    revision

--- 10_gradient.py
+++ 10_gradient.py
@@ -1,7 +1,7 @@
 # 확률적 경사 하강법 (Stochastic Gradient Descent)
 # SGDClassifier
 
-# 데이터 준비
+# 데이터 준비 (전처리, 각 특성마다 스케일을 같게 해야 함)
 import pandas as pd
 fish = pd.read_csv('https://bit.ly/fish_csv_data')
 
@@ -13,22 +13,27 @@ from sklearn.model_selection import train_test_split
 train_input, test_input, train_target, test_target = train_test_split(
     fish_input, fish_target, random_state=42)
 
-# 표준화
+# 표준화 (표준점수)
 from sklearn.preprocessing import StandardScaler
 ss = StandardScaler()
 ss.fit(train_input)
 train_scaled = ss.transform(train_input)
 test_scaled = ss.transform(test_input)
 
+
 # SGDClassifier
 from sklearn.linear_model import SGDClassifier
 sc = SGDClassifier(loss='log_loss', max_iter=10, random_state=42)
+# loss='log_loss': 로지스틱 손실 함수를 나타냄
+# max_iter: 에포크. 훈련 세트를 10번 쓴다는 뜻
 sc.fit(train_scaled, train_target)  
-# 경고! ConvergenceWarning: Maximum number of iteration reached before convergence. 
-# Consider increasing max_iter to improve the fit.
-#  warnings.warn(
-print(sc.score(train_scaled, train_target)) # 0.773109243697479
-print(sc.score(test_scaled, test_target))   # 0.775
+# print(sc.score(train_scaled, train_target)) # 0.773109243697479
+# print(sc.score(test_scaled, test_target))   # 0.775
+
+sc.partial_fit(train_scaled, train_target)
+# 기존에 학습한 w와 b를 그대로 유지하면서 다시 또 훈련하는 것
+print(sc.score(train_scaled, train_target)) # 0.8151260504201681
+print(sc.score(test_scaled, test_target))   # 0.85
 
 
 # 에포크와 과대/과소적합
